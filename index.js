c7388de48f79f991effd9e5a7dd86a9ed86cb2cb
shubham Pati <patilshubham1261@gmail.com>
2025-06-09 00:55:55 +0530

    completed

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
+//require method override for patch and delete actions
+
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 //require path
 const path = require("path");
 
@@ -71,4 +76,31 @@ app.get("/posts/:id", (req,res) => {
     if(!(post)){
         res.render("error.ejs");
     }
+});
+
+app.patch("/posts/:id", (req, res) => {
+    let { id } = req.params;
+    let newContent = req.body.content;
+
+    let post = posts.find((p) => id === p.id);
+    if (post) {
+        post.content = newContent; 
+        res.redirect("/posts"); 
+    } else {
+        res.status(404).send("Post not found");
+    }
+});
+
+
+app.get("/posts/:id/edit",(req,res) => {
+    let {id} = req.params;
+    let post = posts.find((p) => id === p.id);
+    res.render("edit.ejs", {post});
+});
+
+
+app.delete("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id !== p.id);
+    res.redirect("/posts");
 });
